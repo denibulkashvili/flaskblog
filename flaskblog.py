558d1b26437242facdc35449d18bd0ed7002cd4d
denibulkashvili <denibulkashvili@gmail.com>
2018-05-07 13:42:48 +0800

    Removed register functionality for now

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
+# from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'dev'
@@ -29,18 +29,18 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash('Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
+# @app.route("/register", methods=['GET', 'POST'])
+# def register():
+#     form = RegistrationForm()
+#     if form.validate_on_submit():
+#         flash('Account created for {form.username.data}!', 'success')
+#         return redirect(url_for('home'))
+#     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
-def login():
-    form = LoginForm()
-    return render_template('login.html', title='Login', form=form)
+# @app.route("/login")
+# def login():
+#     form = LoginForm()
+#     return render_template('login.html', title='Login', form=form)
 
 # Debug
 if __name__ == '__main__':
