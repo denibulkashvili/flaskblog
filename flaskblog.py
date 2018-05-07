8b76cfd181d9730f41de52fe4df269cf60a25ec8
denibulkashvili <denibulkashvili@gmail.com>
2018-05-07 13:20:32 +0800

    Initial commit of the project

---
+++ flaskblog.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'dev'
+
+posts = [
+    {
+        'author': 'Denis',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'May 5th, 2018'
+    },
+    {
+        'author': 'User',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 5th, 2018'
+    }
+]
+
+# Routes
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+# Debug
+if __name__ == '__main__':
+    app.run(debug=True)
