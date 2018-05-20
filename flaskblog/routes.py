d2e687841eaf77bbcc41b5bd017049390570ce5f
denibulkashvili <denibulkashvili.gmail.com>
2018-05-20 14:32:48 +0800

    updated templates

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,26 +2,12 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog import app
 from flaskblog.models import Topic, Post
 
-posts = [
-    {
-        'author': 'Denis',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'May 5th, 2018'
-    },
-    {
-        'author': 'User',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 5th, 2018'
-    }
-]
 
 # Routes
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template('home.html')
 
 @app.route("/about")
 def about():
