8d3cadb1db962e5b3b9b0ac85fc41e73e137ddce
denibulkashvili <denibulkashvili.gmail.com>
2018-05-20 15:11:32 +0800

    Added admin view with crud support

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dev'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+admin = Admin(app)
+
 
 
 from flaskblog import routes
+from flaskblog.models import Topic, Post
+
+admin.add_view(ModelView(Topic, db.session))
+admin.add_view(ModelView(Post, db.session))
